207630baf31c495b8997b5ac230e94355cf23f3c
Hockenmaier <206103526@TFAYD.com>
2017-06-09 18:38:47 -0700

    removed some unneeded things from app.py

--- app.py
+++ app.py
@@ -8,28 +8,18 @@ import os
 import socket
 
 # Connect to Redis
-redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
+# redis = Redis(host="redis", db=0, socket_connect_timeout=2, socket_timeout=2)
 
 app = Flask(__name__)      
 
 @app.route("/")
 def hello():
-    try:
-        visits = redis.incr("counter")
-    except RedisError:
-        visits = "<i>cannot connect to Redis, counter disabled and this is a new image now!</i>"
-
-    html = "<h3>Hello {name}!</h3>" \
-           "<b>Hostname:</b> {hostname}<br/>" \
-           "<b>Visits:</b> {visits}"
     return render_template('home.html')
 	#return html.format(name=os.getenv("NAME", "world"), hostname=socket.gethostname(), visits=visits)
 
 if __name__ == "__main__":
 	app.run(host='0.0.0.0', port=80)
 
-	
-	
 '''
 from flask import Flask, render_template
  
